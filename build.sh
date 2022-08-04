6c5a2ed34433288d31a3af78a73c892528ea22ab
ThizGuy1016 <vavraacademy@gmail.com>
2022-08-03 19:25:07 -0500

    Updated build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin//sh
+
+COMP_DIR=$(pwd)
+
+cd $(pwd)/src
+SRC_VAR=""
+
+for f in *.cpp; do
+    echo "COMPILING /src/${f}"
+    SRC_VAR+="src/"
+    SRC_VAR+="${f} "
+done
+
+cd $COMP_DIR
+
+set -e
+
+clang++ -g -DNDEBUG -DVLOG -DVLOG_VERBOSE main.cpp ${SRC_VAR} `llvm-config --cxxflags --ldflags --system-libs --libs core` -o voltt
+
+echo "RUNNING voltt"
+./voltt
