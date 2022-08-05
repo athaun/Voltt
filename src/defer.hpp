246cbfa162282ff9e89ed8b4318ea45523630844
ThizGuy1016 <vavraacademy@gmail.com>
2022-08-04 19:17:39 -0500

    implemented defer

---
+++ src/defer.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <functional>
+
+struct defer {
+	std::function<void(void)> m_action;
+
+	explicit defer(const std::function<void(void)>& _act)
+	: m_action(_act)
+	{}
+
+	explicit defer(std::function<void(void)>&& _act)
+	: m_action(std::move(_act))
+
+	~defer() { m_action(); }
+}
