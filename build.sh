5a4837e17cad69959d83c7ac5dab297cff7ed09f
ThizGuy1016 <vavraacademy@gmail.com>
2022-08-04 20:06:02 -0500

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,11 @@
 #!/usr/bin//sh
 
-COMP_DIR=$(pwd)
-
-cd $(pwd)/src
-SRC_VAR=""
-
-for f in *.cpp; do
-    echo "COMPILING /src/${f}"
-    SRC_VAR+="src/"
-    SRC_VAR+="${f} "
-done
-
-cd $COMP_DIR
+CPP_FILES=$(find src/ -type f -name '*cpp')
+echo "$CPP_FILES"
 
 set -e
 
-clang++ -g -DNDEBUG -DVLOG -DVLOG_VERBOSE ${SRC_VAR} `llvm-config --cxxflags --ldflags --system-libs --libs core` -o voltt
+clang++ -g -DNDEBUG -DVLOG -DVLOG_VERBOSE ${CPP_FILES} `llvm-config --cxxflags --ldflags --system-libs --libs core` -o voltt
 
 echo "RUNNING voltt"
 ./voltt
